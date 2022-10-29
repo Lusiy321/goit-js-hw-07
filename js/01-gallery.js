c7f5ddeea3db8e6c902080437947ed6ec33a3222
Volodymyr Lysenko <lusiy321@gmail.com>
2022-10-29 21:59:27 +0300

    Summ2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line;
 
-console.log(galleryItems);
+const divEl = document.querySelector(".gallery");
+
+for (const gallery of galleryItems) {
+  const imgEl = `<div class="gallery__item">
+  <a class="gallery__link" href="${gallery.original}">
+  <img src="${gallery.preview}" alt="${gallery.description}" class="gallery__image" data-source="${gallery.original}">
+  </a></div>`;
+  divEl.insertAdjacentHTML("beforeend", imgEl);
+}
+
+divEl.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${evt.target.dataset.source}">
+	`
+    )
+    .show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const liEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+for (const gallery of galleryItems) {
+  const imgEl = `<li class="gallery__item">
+  <a class="gallery__link" href="${gallery.original}">
+  <img src="${gallery.preview}" alt="${gallery.description}" class="gallery__image" >
+  </a></li>`;
+  liEl.insertAdjacentHTML("beforeend", imgEl);
+}
+new SimpleLightbox(".gallery__item a", {
+  fadeSpeed: 250,
+  captionsData: "alt",
+});
